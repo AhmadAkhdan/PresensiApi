58a924ebc0991d285a6eb2f0af96fd86b804d2f5
AhmadAkhdan <AhmadAkhdan1705@gmail.com>
2026-05-25 18:55:19 +0700

    M7-P3

---
+++ src/main/java/com/example/PresensiApi/MahasiswaRepository.java
@@ -0,0 +1,6 @@
+package com.example.PresensiApi;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MahasiswaRepository extends JpaRepository<Mahasiswa, String> {
+
+}
